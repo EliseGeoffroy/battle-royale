1b9def7ceeaf0670a8afb07809bf0b14018830a7
Elise Geoffroy <elise.geoffroy03@gmail.com>
2024-09-23 18:30:13 +0200

    add currcharac in seed.php

--- data/seed.php
+++ data/seed.php
@@ -2,50 +2,50 @@
 
 require_once('../database/pdoOpen.php');
 
-// //weapon table
-// $arrayWeaponList = json_decode(file_get_contents('./WeaponList.json'), true);
+//weapon table
+$arrayWeaponList = json_decode(file_get_contents('./WeaponList.json'), true);
 
-// $statement = $pdo->prepare("CREATE TABLE weapon  (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(45), picture VARCHAR(100), strength INT, class VARCHAR(45), PRIMARY KEY (id)) ");
-// $statement->execute();
+$statement = $pdo->prepare("CREATE TABLE weapon  (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(45), picture VARCHAR(100), strength INT, class VARCHAR(45), PRIMARY KEY (id)) ");
+$statement->execute();
 
-// $statement = $pdo->prepare("INSERT INTO weapon (id, name, picture, strength, class) VALUES (DEFAULT, :name, :picture, :strength, :class)");
+$statement = $pdo->prepare("INSERT INTO weapon (id, name, picture, strength, class) VALUES (DEFAULT, :name, :picture, :strength, :class)");
 
-// $name = '';
-// $picture = '';
-// $strength = '';
-// $class = '';
+$name = '';
+$picture = '';
+$strength = '';
+$class = '';
 
-// $statement->bindParam(':name', $name);
-// $statement->bindParam(':picture', $picture);
-// $statement->bindParam(':strength', $strength);
-// $statement->bindParam(':class', $class);
+$statement->bindParam(':name', $name);
+$statement->bindParam(':picture', $picture);
+$statement->bindParam(':strength', $strength);
+$statement->bindParam(':class', $class);
 
-// foreach ($arrayWeaponList as $weapon) {
-//     $name = $weapon['name'];
-//     $picture = $weapon['picture'];
-//     $strength = $weapon['strength'];
-//     $class = $weapon['class'];
+foreach ($arrayWeaponList as $weapon) {
+    $name = $weapon['name'];
+    $picture = $weapon['picture'];
+    $strength = $weapon['strength'];
+    $class = $weapon['class'];
 
-//     $statement->execute();
-// }
+    $statement->execute();
+}
 
-//character table
+//characterList table
 $arrayCharacList = json_decode(file_get_contents('./characList.json'), true);
 
-// $statement = $pdo->prepare("CREATE TABLE `battleroyale`.`character` (
-//   `id` INT NOT NULL AUTO_INCREMENT,
-//   `name` VARCHAR(45) NOT NULL,
-//   `picture` VARCHAR(100) NOT NULL,
-//   `strength` INT NOT NULL,
-//   `defense` INT NOT NULL,
-//   `health` INT NOT NULL,
-//   `class` VARCHAR(20) NOT NULL,
-//   `status` VARCHAR(20) NOT NULL,
-//   PRIMARY KEY (`id`));");
+$statement = $pdo->prepare("CREATE TABLE `battleroyale`.`characterList` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `picture` VARCHAR(100) NOT NULL,
+  `strength` INT NOT NULL,
+  `defense` INT NOT NULL,
+  `health` INT NOT NULL,
+  `class` VARCHAR(20) NOT NULL,
+  `status` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`id`));");
 
-// $statement->execute();
+$statement->execute();
 
-$statement = $pdo->prepare("INSERT INTO `battleroyale`.`character` (`name`, `picture`, `strength`, `defense`, `health`, `class`, `status`) VALUES (:name, :picture, :strength, :defense, :health, :class, :status);");
+$statement = $pdo->prepare("INSERT INTO `battleroyale`.`characterList` (`name`, `picture`, `strength`, `defense`, `health`, `class`, `status`) VALUES (:name, :picture, :strength, :defense, :health, :class, :status);");
 
 $name = '';
 $picture = '';
@@ -71,13 +71,10 @@ foreach ($arrayCharacList as $charac) {
     $health = $charac['health'];
     $class = $charac['class'];
 
-    echo $name . $picture . $strength . $defense . $defense . $health . $class . $status;
-    echo '<pre>';
-    print_r($charac);
-
-
-    echo '</pre>';
-
-
     $statement->execute();
 }
+
+//current character table
+
+$statement = $pdo->prepare("CREATE TABLE currcharac (`numPerso` INT NOTNULL, `name` VARCHAR(45), `picture` VARCHAR(100), `currHealth` INT, `totalHealth` INT, `currStrength` INT, `currDefense` INT, `class` VARCHAR(20), `idWeapon` INT)");
+$statement->execute();
